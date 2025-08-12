6fafdcfafc40c219b2de3f78376ff44fbe936297
chetkelley <kchest@gmail.com>
2025-08-12 08:00:52 +0200

    modified add_titles.js

--- schema.sql
+++ schema.sql
@@ -9,3 +9,12 @@ CREATE TABLE IF NOT EXISTS description (
   text TEXT NOT NULL,
   timestamp TIMESTAMPTZ DEFAULT NOW()
 );
+
+CREATE TABLE IF NOT EXISTS "session" (
+  sid varchar NOT NULL COLLATE "default",
+  sess json NOT NULL,
+  expire timestamp(6) NOT NULL,
+  CONSTRAINT "session_pkey" PRIMARY KEY ("sid")
+) WITH (OIDS=FALSE);
+
+CREATE INDEX IF NOT EXISTS "IDX_session_expire" ON "session" ("expire");
