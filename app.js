e68c589959b7076678b4df113d8eb5c39ea4d0b7
chetkelley <kchest@gmail.com>
2025-11-09 21:43:47 +0100

    updated app.listen to include 0.0.0.0

--- app.js
+++ app.js
@@ -84,6 +84,6 @@ app.post('/photo/:id', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server started on port ${PORT}`);
 });
